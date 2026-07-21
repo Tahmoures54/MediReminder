45c4c8e9c2fc989b7b8a2c3d0e13f8e1ad5dbc4d
Tahmoures54 <37265001+Tahmoures54@users.noreply.github.com>
2026-07-21 17:08:24 +0330

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tahmoures.medireminder',
-  appName: 'دارویادآور',
+  appName: 'MediReminder',
   webDir: 'dist',
   bundledWebRuntime: false,
 };
